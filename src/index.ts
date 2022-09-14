c02c176cc2080cc16da9e7374f2915ee8c2c901e
David Landes <davidkailandes@gmail.com>
2022-09-14 16:57:12 -0400

    added new file called LinkedList- Code implemented, Missing refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 import { Sorter } from "./Sorter";
-import { NumberCollection } from "./NumbersCollection";
+import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollections } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
-const numbersCollection = new NumberCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+// const charactersCollection = new CharactersCollections("Xaayb");
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(numbersCollection.data);
+linkedList.print();
